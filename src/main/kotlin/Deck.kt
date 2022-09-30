a3821f805d62211f320a53d44c132bb9d5ad21f6
Amanda Maifeld <a.maifeld@hotmail.com>
2022-09-30 16:52:40 -0700

    adding rest of deck functionality

--- src/main/kotlin/Deck.kt
+++ src/main/kotlin/Deck.kt
@@ -1,6 +1,16 @@
+import java.util.*
+
 class Deck {
 
     val cards = Array(52) { Card(it % 13, getSuit(it)) }
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard():Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck - cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
 
     private fun getSuit(i: Int) = when(i / 13) {
             0 -> "Clubs"
@@ -8,4 +18,6 @@ class Deck {
             2 -> "Hearts"
             else -> "Spades"
         }
+
+
 }
